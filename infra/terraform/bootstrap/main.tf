7ba40066a0e4c9bfd5a7c2a4b2fe971259a2515d
collinbentley1 <collin.bentley@me.com>
2026-08-21 13:34:40 -0400

    chore: repin hardened platform

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=823466fd2920a1539f5337409c6b59da34700e8d"
+  source = "github.com/collinbentley1/platform//terraform/modules/bootstrap?ref=95ad7531492cba2891ec6d02fa2d821955cd36bc"
 
   app                         = "runsetta"
   project_id                  = var.project_id
@@ -12,20 +12,20 @@ module "bootstrap" {
   github_owner_id             = var.github_owner_id
   github_repository_id        = var.github_repository_id
   trusted_platform_workflow_shas = [
-    "823466fd2920a1539f5337409c6b59da34700e8d",
+    "95ad7531492cba2891ec6d02fa2d821955cd36bc",
   ]
   required_services = [
     "artifactregistry.googleapis.com",
     "cloudresourcemanager.googleapis.com",
     "iam.googleapis.com",
     "iamcredentials.googleapis.com",
-    "orgpolicy.googleapis.com",
     "run.googleapis.com",
     "secretmanager.googleapis.com",
     "serviceusage.googleapis.com",
     "storage.googleapis.com",
     "sts.googleapis.com",
   ]
-  legacy_compatibility_mode = false
-  runtime_description       = "Runtime identity for the Runsetta Cloud Run services."
+  legacy_compatibility_mode                              = false
+  manage_automatic_default_service_account_grants_policy = var.manage_automatic_default_service_account_grants_policy
+  runtime_description                                    = "Runtime identity for the Runsetta Cloud Run services."
 }
